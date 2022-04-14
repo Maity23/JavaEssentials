14b3c5d8ae3f7d50600d523bd29706fcf3a13c5a
kmaity <kaushik.maity@blackboard.com>
2022-04-14 16:02:13 +0530

    My Code Change

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+    public static void main(String [] args){
+        int result = 10+20;
+        String answer ="The answer is" + " " +result;
+        System.out.println(answer);
+
+        String howMany= 20+ " " + "things";
+        System.out.println(howMany);
+
+        int intValue=42;
+        var fromInt=Integer.toString(intValue);
+        System.out.println(fromInt);
+    }
+}
